8059d99db83de7b1776ee786e055665f790d10ce
Piyush Jayawant Rewatkar <piyushjayawantrewatkar@Piyushs-MacBook-Pro.local>
2025-09-02 10:46:12 -0700

    UI for editing the image

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import BadgeEditor from './components/BadgeEditor';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Badge Generator</h1>
+      </header>
+      <BadgeEditor />
+    </div>
+  );
+}
+
+export default App;
